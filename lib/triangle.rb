2cebcb7ba0d6d3ab3c0199240497e8b93ee18efc
reduviidae <wiljago@gmail.com>
2018-12-17 16:42:44 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z, :kind
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+
+    case
+    when x + y < z || z + y < x || x + z < y
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    when x == y && y == z && x == z
+      @kind = :equilateral
+    when x == y || y == z || x == z
+      @kind = :isosceles
+    when x != y && y != z && z != x
+      @kind = :scalene
+    when x == 0 || y == 0 || z == 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is not a triangle."
+    end
+  end
 end
